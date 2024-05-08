99ddf709c9630fc82012a30a70cbdaedee80f3a8
Terry Sigle <terrysigle@pingidentity.com>
2024-05-08 09:41:13 -0500

    fix docs

--- PingIdentityBaseDaVinciRulePack.js
+++ PingIdentityBaseDaVinciRulePack.js
@@ -1,14 +1,21 @@
 const LintRulePack = require("pingone-davinci-linter/lib/LintRulePack");
-const { version } = require("./package.json");
+const {
+  name,
+  version,
+  description,
+  homepage,
+  author,
+} = require("./package.json");
 
 class PingIdentityBaseDaVinciRulePack extends LintRulePack {
   constructor() {
     super({
       directory: __dirname,
       version,
-      name: "pingidentity-base-davinci-rule-pack",
-      description: "Collection of base rules used to lint DaVinci flows.",
-      author: "Ping Identity - cloud-solutions@pingidentity.com",
+      name,
+      description,
+      homepage,
+      author,
     });
   }
 }
